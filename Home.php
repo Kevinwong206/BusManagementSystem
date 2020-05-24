2aded197955c16fe0e3f3a73edaf7595228063d7
JovenTung <65539698+JovenTung@users.noreply.github.com>
2020-05-24 16:48:12 +0800

    Update Home.php
    Edited alignment of content and footer

--- Home.php
+++ Home.php
@@ -6,7 +6,7 @@
 <meta charset=utf-8>
 </head>
 
-<body>
+<body a link="black" vlink="black">
     
         <?php 
         echo "<div id='container'>";
@@ -36,7 +36,7 @@
         echo "<p>&nbsp;</p>";
         echo "<p>&nbsp;</p> ";
 		
-				echo "<h1><center>Popular Places to go in Penang</center></h1>";
+				echo "<h1><center><font face = 'Showcard Gothic'><big>Popular Places to go in Penang</center></big></font></h1>";
 			
         echo "</h1>";
 
@@ -47,27 +47,29 @@
 			
             echo "<td><img src='images/KekLokSi.jpg' height='300' width='500'></td>";
 			echo "<td>";
-			echo "<a href = 'http://www.penang.ws/penang-attractions/kek-lok-si.htm'><font face = 'Showcard Gothic'><big>Kek Lok Si </big></font></a>";
+			echo "<a href = 'http://www.penang.ws/penang-attractions/kek-lok-si.htm'><font face = 'Showcard Gothic'>Kek Lok Si</font></a>";
 			echo "<br/><br/>";
-			echo "<b>Address:</b> Pekan Ayer Itam, 11500 Air Itam, Penang";
+			echo "<h5 align='left'><b>Address:</b> Pekan Ayer Itam, 11500 Air Itam, Penang</h5>";
 			echo "<br/>";
-			echo "<b>Phone:</b> 04-828 3317";
-			echo "<br/><br/>";
-            echo "The Kek Lok Si Temple is a Buddhist temple situated in Air Itam, Penang, Malaysia. It is the largest Buddhist temple in Malaysia, 
+			echo "<h5 align='left'><b>Phone:</b> 04-828 3317</h5>";
+			echo "<br/>";
+            echo "<h6>The Kek Lok Si Temple is a Buddhist temple situated in Air Itam, Penang, Malaysia. It is the largest Buddhist temple in Malaysia, 
 				  and is also an important pilgrimage centre for Buddhists from Hong Kong, the Philippines, Singapore and other countries in Southeast Asia..";
+			echo "</h6>";
 			echo "</td>";
         echo "</tr>";
         
 		echo "<tr>";
             echo "<td>";
-			echo "<a href = 'http://penanghill.gov.my/index.php/en/'><font face = 'Showcard Gothic'><big>Penang Hill</big></font></a>";
+			echo "<a href = 'http://penanghill.gov.my/index.php/en/'><font face = 'Showcard Gothic'>Penang Hill</font></a>";
 			echo "<br/><br/>";
-			echo "<b>Address:</b> Perbadanan Bukit Bendera Pulau Pinang,Tingkat 7, Bangunan PBBPP,Jalan Stesen Bukit Bendera,Air Itam, 11500 Pulau Pinang.";
+			echo "<h5 align='left'> <b> Address: </b>Jalan Stesen Bukit Bendera,Air Itam, 11500 Pulau Pinang.</h5>";
 			echo "<br/>";
-			echo "<b>Email:</b>inquiries@penanghill.gov.my";
-			echo "<br/><br/>";
-            echo "Penang Hill is a hill resort comprising a group of peaks on Penang Island, Malaysia. It is located within the Air Itam suburb, 
+			echo "<h5 align='left'>Email:inquiries@penanghill.gov.my</h5>";
+			echo "<br/>";
+            echo "<h6>Penang Hill is a hill resort comprising a group of peaks on Penang Island, Malaysia. It is located within the Air Itam suburb, 
 				  9 km west of the center of George Town. Penang Hill is also known by the Malay name Bukit Bendera, which actually refers to Flagstaff Hill, the most developed peak";
+			echo "</h6>";		
 			echo "</td>";
 			echo "<td><img src='images/PenangHill.png' height='300' width='500'></td>";
         echo "</tr>";
@@ -76,29 +78,31 @@
 			
             echo "<td><img src='images/entopia.jpg' height='300' width='500'></td>";
 			echo "<td>";
-			echo "<a href = 'http://www.entopia.com'><font face = 'Showcard Gothic'><big>Entopia</big></font></a>";
+			echo "<a href = 'http://www.entopia.com'><font face = 'Showcard Gothic'>Entopia</font></a>";
 			echo "<br/><br/>";
-			echo "<b>Address:</b> No.830, Jalan Teluk Bahang, Teluk Bahang, 11050, Pulau Pinang";
+			echo "<h5 align='left'><b>Address:</b> No.830, Jalan Teluk Bahang, Teluk Bahang, 11050, Pulau Pinang </h5>";
 			echo "<br/>";
-			echo "<b>Phone:</b> 04-888 8111";
-			echo "<br/><br/>";
-            echo "Entopia, a tropical sanctuary for our planet's little denizens from those that fly freely in the sky to the ones that creep stealthily beneath the ground. 
+			echo "<h5 align='left'><b>Phone:</b> 04-888 8111 </h5>";
+			echo "<br/>";
+            echo "<h6>Entopia, a tropical sanctuary for our planet's little denizens from those that fly freely in the sky to the ones that creep stealthily beneath the ground. 
 				  Journey into a magical paradise that honours the unsung heroes of our natural world with plenty to discover from outdoor adventures to cavernous mysteries 
 				  and a multi-storey Indoor Discovery Centre.";
+			echo "</h6>";		
 			echo "</td>";
         echo "</tr>";
 		
 		echo "<tr>";
             echo "<td>";
-			echo "<a href = 'https://www.timetravelturtle.com/george-town-heritage-penang-malaysia/'><font face = 'Showcard Gothic'><big>Penang Heritage Area</big></font></a>";
+			echo "<a href = 'https://www.timetravelturtle.com/george-town-heritage-penang-malaysia/'><font face = 'Showcard Gothic'>Penang Heritage Area</font></a>";
 			echo "<br/><br/>";
-			echo "<b>Address:</b> Georgetown, 10200 George Town, Penang";
+			echo "<h5 align='left'><b>Address:</b> Georgetown, 10200 George Town, Penang </h5>";
 			echo "<br/>";
-			echo "<b>Phone:</b> 04-261 6606";
-			echo "<br/><br/>";
-            echo "The George Town UNESCO World Heritage Site features Malaysia’s best restored and re-adaptive traditional architecture as well as a multitude of heritage trades and arts.
+			echo "<h5 align='left'><b>Phone:</b> 04-261 6606 </h5>";
+			echo "<br/>";
+            echo "<h6>The George Town UNESCO World Heritage Site features Malaysia’s best restored and re-adaptive traditional architecture as well as a multitude of heritage trades and arts.
 				  The world-renowned ‘Street of Harmony’ reflects the influence of the diverse communities that have made Penang their home. At only 2.5sq km, the George Town World Heritage 
 				  Site is compact enough to be explored on foot, bicycle or trishaw.";
+			echo "</h6>";		
 			echo "</td>";
 			echo "<td><img src='images/heritage.jpg' height='300' width='500'></td>";
         echo "</tr>";
@@ -107,7 +111,7 @@
 		
 		//FOOTER
 		echo "<div id='footer'>";
-		echo "<p>Call Us Now : 0123456789 </p>";
+		echo "<p>Bus2U Hotline : 0123456789 </p>";
 		echo "</div>";//end of footer
        	
         echo "</div>";// end the css of the content

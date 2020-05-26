6c1ed9e165330309ceb57001d189952a8906cc04
Banana Fries <65538857+chuanhao0705@users.noreply.github.com>
2020-05-26 11:22:54 +0800

    Update Purchase.php

--- Purchase.php
+++ Purchase.php
@@ -2,7 +2,7 @@
 
 <head>
 <link rel="stylesheet" type="text/css" href="style.css" />
-<title>Bus2u Home Page</title>
+<title>Bus2u Bus Ticket Purchasing Page</title>
 <meta charset=utf-8>
 </head>
 
